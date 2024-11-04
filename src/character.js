48400bf8554b050e3fc264d2e3272403ecfba425
VladDadonkin <vlad.dadonkin.02@mail.ru>
2024-11-04 15:16:11 +0300

    add methods

--- src/character.js
+++ src/character.js
@@ -9,12 +9,34 @@ export default class Character {
             throw new Error(`Выберите тип из ${validTypes.join(', ')}`);
         }
 
-
+        
 
         this.name = name;
         this.type = type;
         this.level = 1;
         this.health = 100;
+        this.attack = 0;
+        this.defence = 0;        
     }
 
+    levelUp() {
+        
+        if (this.health === 0) {;
+            throw new Error('нельзя повысить левел умершего');
+        }
+            this.level++;
+            this.attack *= 1.2;
+            this.defence *= 1.2;
+            this.health = 100;
+    };
+
+    damage(points) {
+
+        if (this.health >= 0){
+            health -= points * (1 - defence / 100);
+        };
+        if (this.health < 0) {
+            this.health = 0;
+        };
+    };
 }
